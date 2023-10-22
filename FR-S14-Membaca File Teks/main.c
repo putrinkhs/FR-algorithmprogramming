0be48f860a38c8551279d87a383033bd20b9a8de
Putri Nur Khasanah <143639372+putrinkhs@users.noreply.github.com>
2023-10-22 23:45:32 +0700

    Create main.c

---
+++ FR-S14-Membaca File Teks/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+// Fungsi untuk membaca dan menampilkan data dari file teks
+void readAndDisplayData() {
+    FILE *file;
+    char line[100];  // Maksimum panjang baris
+    char *token;
+
+    // Membuka file untuk dibaca
+    file = fopen("data.txt", "r");
+
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return;
+    }
+
+    printf("=====================================\n");
+    printf("PROGRAM MEMBACA FILE TEKS\n");
+    printf("=====================================\n");
+
+    // Membaca dan menampilkan setiap baris dalam file
+    while (fgets(line, sizeof(line), file)) {
+        // Membaca data menggunakan strtok
+        token = strtok(line, " ");  // Membaca ID
+        printf("ID: %s\n", token);
+
+        token = strtok(NULL, " ");  // Membaca nama depan
+        printf("Nama Depan: %s\n", token);
+
+        // Membaca nama belakang (memanfaatkan sisa baris)
+        char full_name[50] = "";
+        while (token = strtok(NULL, " ")) {
+            strcat(full_name, token);
+            strcat(full_name, " ");
+        }
+        printf("Nama Belakang: %s\n", full_name);
+
+        token = strtok(NULL, "\n");  // Membaca jumlah uang
+        printf("Jumlah Uang: %s\n");
+
+        printf("=====================================\n");
+    }
+
+    // Menutup file setelah selesai membaca
+    fclose(file);
+}
+
+int main() {
+    readAndDisplayData();
+    return 0;
+}
